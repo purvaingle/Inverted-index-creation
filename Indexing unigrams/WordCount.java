0a2b83b22111817c063e6e2419b958f0b742eaa1
Purva Satish Ingle <90026828+purvaingle@users.noreply.github.com>
2023-11-19 22:35:33 -0800

    Add files via upload

---
+++ Indexing unigrams/WordCount.java
@@ -0,0 +1,90 @@
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import java.util.HashMap;
+
+
+public class WordCount {
+  public static class TokenizerMapper extends Mapper<Object, Text, Text, Text> {
+    private Text doc = new Text();
+    private Text word = new Text();
+
+    public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+      String[] parts = value.toString().split("\t", 2);
+      doc.set(parts[0]);
+      String content = parts[1].toLowerCase().replaceAll("[^a-z\\s]", " ").replaceAll("\\s+", " ");
+
+      StringTokenizer itr = new StringTokenizer(content);
+      while (itr.hasMoreTokens()) {
+        word.set(itr.nextToken());
+        context.write(word, doc);
+      }
+    }
+  }
+
+  public static class HReducer extends Reducer<Text, Text, Text, Text> {
+    private Text result = new Text();
+
+    // private IntWritable result = new IntWritable();
+    public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+      HashMap<String, Integer> wordFrequencies = new HashMap<>();
+
+      for (Text word : values) {
+          String wordString = word.toString();
+          // Checking if the word is in HashMap
+          if (wordFrequencies.containsKey(wordString)) {
+              // If yes, count++
+              wordFrequencies.put(wordString, wordFrequencies.get(wordString) + 1);
+          } else {
+              wordFrequencies.put(wordString, 1);
+          }
+      }
+
+      StringBuilder combinedText = new StringBuilder(); 
+      for (String word : wordFrequencies.keySet()) {
+          combinedText.append(word)
+                      .append(":")
+                      .append(String.valueOf(wordFrequencies.get(word)))
+                      .append("\t");
+      }
+
+      result.set(combinedText.substring(0, combinedText.length() - 1)); 
+      context.write(key, result);
+
+      // int sum = 0;
+      // for (IntWritable val : values)
+      // {
+      // sum += val.get();
+      // }
+      // result.set(sum);
+      // context.write(key, result);
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    Configuration conf = new Configuration();
+    Job job = Job.getInstance(conf, "Unigram index");
+
+    job.setJarByClass(WordCount.class);
+    job.setMapperClass(TokenizerMapper.class);
+    job.setReducerClass(HReducer.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(Text.class);
+
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+    System.exit(job.waitForCompletion(true) ? 0 : 1);
+  }
+}// WordCount
